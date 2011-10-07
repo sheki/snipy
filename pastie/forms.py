7664d2db13a3ddbdabf62ef64e9241bad9e90dd7
Abhishek Kona <abhishek.kona@gmail.com>
2011-10-07 22:59:45 +0530

    forms for selecting language

--- pastie/forms.py
+++ pastie/forms.py
@@ -2,5 +2,9 @@ from flaskext import wtf
 from flaskext.wtf import validators
 
 class PostForm(wtf.Form):
-    title = wtf.TextField('Snippet Name', validators=[validators.Required()],)
-    content = wtf.TextAreaField('code', validators=[validators.Required()])
+    languages = ['Java','Python','C','C++','Perl','HTML']
+    langTuples = [ (str(i),languages[i])for i in range(0,len(languages)) ]
+    name = wtf.TextField('name', validators=[validators.Required()],)
+    code = wtf.TextAreaField('code', validators=[validators.Required()])
+    lang  = wtf.SelectField('language',choices=langTuples)
+
